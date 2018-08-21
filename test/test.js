6c8372f4f1cf02095edec1708af5b16150dea81f
psyperl <28307592+psyperl@users.noreply.github.com>
2018-08-21 21:15:51 +0700

    Added dependencies and test case for uuidv5 for #36 (#49)
    * Added dependencies for uuid version 3.3.2 (#36)
    
    * Add test case for uuidv5 (#36)

--- test/test.js
+++ test/test.js
@@ -8,3 +8,14 @@ describe('This is just for testing the test ;p', function() {
 });
 
 
+describe("Encryption Function", 
+    uuid => describe("uuidv5 encryption", 
+        test_uuid => {
+            it("should return the corresponding value for uuidv5 with sitename as a namespace", function(){
+                const uuidv5 = require('uuid/v5')
+                const VE_NAMESPACE = uuidv5("peaceful-peak-41153.herokuapp.com", uuidv5.DNS)
+                assert.equal(uuidv5("Hello World!", VE_NAMESPACE), "9312fccf-890c-58a5-89ed-57a0fcf98596")
+            })
+        }
+    )
+)
