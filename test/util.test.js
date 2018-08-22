97829daa50ab357441eb06ce4de96a1e987f48f2
Jun Yada <41787838+junyada100@users.noreply.github.com>
2018-08-22 19:50:40 +0700

    Fix#42 Add utility function for encoding image (#54)

--- test/util.test.js
+++ test/util.test.js
@@ -1,11 +1,13 @@
-const { expect } = require('chai');
+const { expect } = require('chai')
+  .use(require('chai-bytes'));
 const util = require('../util');
-
-let txt = 'password12345';
-let hash = "";
+const fs = require('fs');
 
 describe("./util/index.js - Utility functions", () => {
   describe("password encrypt functions", () => {
+    let txt = 'demo1';
+    let hash = "";
+
     it("should encrypt plain password", () => {
       hash = util.encrypt(txt);
       expect(hash.length).to.be.equal(60);
@@ -14,6 +16,20 @@ describe("./util/index.js - Utility functions", () => {
     it("should compare plain password with hash", () => {
       expect(util.comparePassword(txt, hash)).to.be.true;
       expect(util.comparePassword("password", hash)).to.be.false;
+    });
+  });
+
+  describe("File encoding functions", () => {
+    let encoded_txt = "";
+    let srcBuffer = fs.readFileSync("./static/img/person.png");
+    it("should be able to encoding file", () => {
+      encoded_txt = util.encodeFile(srcBuffer);
+      expect(encoded_txt).is.not.empty;
+    });
+    it("should be able to decoding file", () => {
+      let destBuffer = util.decodeFile(encoded_txt);
+      expect(destBuffer).is.not.empty;
+      expect(destBuffer).is.equalBytes(srcBuffer);
     });
   });
 });
--- util/index.js
+++ util/index.js
@@ -12,4 +12,14 @@ util.comparePassword = (plain_password, hash_password) => {
   return bcrypt.compareSync(plain_password, hash_password);
 };
 
+//pass Buffer object, return Base64 encoded string
+util.encodeFile = (buf) => {
+  return buf.toString('base64');
+};
+
+//pass Base64 encoded string, return Buffer object
+util.decodeFile = (encoded_txt) => {
+  return Buffer.from(encoded_txt, 'base64');
+};
+
 module.exports = util;
