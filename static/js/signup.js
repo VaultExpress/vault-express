fbc887f1f60856bca9d71a9855ca205aa4521092
Jun Yada <41787838+junyada100@users.noreply.github.com>
2018-09-08 12:48:17 +0700

    Fix#28 Make it responsive on mobile (#85)

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,19 +1,3 @@
 (function () {
-  $(document).ready(function() {
-    
-  // To Slide Down repeat password
-  $(".hide").hide();
-  $("input[name='password']").focus(function() {
-  $("#password_repeat").slideDown();
-  });
-
-  // To give
-  $("input").on("focus",function() {
-  $(this).parent().removeClass('input-blur').addClass('input-focus');
-  });
-  $("input").on("blur", function() {
-  $(this).parent().removeClass('input-focus').addClass('input-blur');
-  });
-  });
 
 })();
