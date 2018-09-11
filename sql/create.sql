dd736a7e7fbd73d51bffcf9574bcdc564cc3c7b8
Jun Yada <41787838+junyada100@users.noreply.github.com>
2018-09-11 11:07:12 +0700

    Fix#26 Edit SQL script (sql/create.sql) (#94)

--- sql/create.sql
+++ sql/create.sql
@@ -1,13 +1,29 @@
-CREATE TABLE users (
-  user_id       varchar(36) PRIMARY KEY,
-  username      varchar(50) NOT NULL,
-  password      varchar(60) NOT NULL,
-  email         varchar(50) NOT NULL,
-  display_name  varchar(50) NULL,
-  profile_image varchar(50) NULL,
-  info1         varchar(50) NULL,
-  info2         varchar(50) NULL,
-  info3         varchar(50) NULL,
-  info4         varchar(50) NULL,
-  info5         varchar(50) NULL,
+CREATE TABLE ve_users (
+  id            VARCHAR(36) PRIMARY KEY,
+  username      VARCHAR(15) NOT NULL,
+  password      VARCHAR(60) NOT NULL,
+  provider      VARCHAR(20) NOT NULL,
+  display_name  VARCHAR(50) NULL,
+  given_name    VARCHAR(30) NULL,
+  middle_name   VARCHAR(30) NULL,
+  family_name   VARCHAR(30) NULL,
+  info1         VARCHAR(100) NULL,
+  info2         VARCHAR(100) NULL,
+  info3         VARCHAR(100) NULL,
+  info4         VARCHAR(100) NULL,
+  info5         VARCHAR(100) NULL,
+);
+
+CREATE TABLE ve_emails (
+  id            SERIAL  PRIMARY KEY,
+  email_type    VARCHAR(20) NOT NULL,
+  email_address VARCHAR(254) NOT NULL,
+  user_id       VARCHAR(36) NOT NULL REFERENCES users
+);
+
+CREATE TABLE ve_photos (
+  id            SERIAL  PRIMARY KEY,
+  photo_type    VARCHAR(20) NOT NULL,
+  photo_url     VARCHAR(2000) NOT NULL,
+  user_id       VARCHAR(36) NOT NULL REFERENCES users
 );
--- sql/drop.sql
+++ sql/drop.sql
@@ -1,2 +1,8 @@
-drop table users;
+DO $$ DECLARE
+  r RECORD;
+BEGIN
+  FOR r IN (SELECT tablename FROM pg_tables WHERE tablename like 've_%' AND schemaname = current_schema()) LOOP
+    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+  END LOOP;
+END $$;
 
